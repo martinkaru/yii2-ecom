84544d7576715ead9d1d52cf7ea7b8537028b287
martinkaru <martinkaru@gmail.com>
2014-10-07 14:34:57 +0300

    Attribute interface

--- lib/models/BasketItemInterface.php
+++ lib/models/BasketItemInterface.php
@@ -35,4 +35,19 @@ interface BasketItemInterface extends \Serializable
      * @return string[]
      */
     public function getItemErrors();
+
+    /**
+     * Sets the named attribute value.
+     * @param string $name the attribute name.
+     * @param mixed $value the attribute value.
+     * @see hasAttribute()
+     */
+    public function setAttribute($name, $value);
+
+    /**
+     * Returns a value indicating whether the record has an attribute with the specified name.
+     * @param string $name the name of the attribute
+     * @return boolean whether the record has an attribute with the specified name.
+     */
+    public function hasAttribute($name);
 }
